6b503ee1fc8200645105f074b3fb6c5e0868619c
Oliver <oliver@mail.com>
2025-10-06 11:13:27 -0300

    Modificando para cubo 3d

--- cubo_a_fazer/cubo_a_fazer/cubo.js
+++ cubo_a_fazer/cubo_a_fazer/cubo.js
@@ -1,50 +1,127 @@
-
 var canvas;
 var gl;
 
-var NumVertices  = 36;
+var NumVertices = 36;
 
-var sh_vertices = [];
-var sh_colors = [];
+var pointsArray = [];
+var colorsArray = [];
 
-window.onload = function init()
-{
-    canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+var vertices = [
+    vec4( -0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5, -0.5, -0.5, 1.0 )
+];
 
-    //criar o cubo
+var vertexColors = [
+    vec4(0.0, 0.0, 0.0, 1.0),
+    vec4(1.0, 0.0, 0.0, 1.0),
+    vec4(1.0, 1.0, 0.0, 1.0),
+    vec4(0.0, 1.0, 0.0, 1.0), 
+    vec4(0.0, 0.0, 1.0, 1.0),
+    vec4(1.0, 0.0, 1.0, 1.0), 
+    vec4(0.0, 1.0, 1.0, 1.0),
+    vec4(1.0, 1.0, 1.0, 1.0)
+];
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
     gl.enable(gl.DEPTH_TEST);
 
-    //  Linka e compila os shaders
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    colorCube();
 
-    //Vértices
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(sh_vertices), gl.STATIC_DRAW );    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    
-    //...
-        
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+
+
+    eye = vec3(0.0, 0.0, 3.0);
+    at = vec3(0.0, 0.0, 0.0);
+    up = vec3(0.0, 1.0, 0.0);
+
+
+    theta = [0, 0, 0];
+    axis = 0;
+
     render();
 }
 
-function render()
-{
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+function quad(a, b, c, d) {
 
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+    var indices = [a, b, c, a, c, d];
+    for (var i = 0; i < indices.length; ++i) {
+        pointsArray.push(vertices[indices[i]]);
 
-    requestAnimFrame( render );
+        colorsArray.push(vertexColors[indices[i]]);
+    }
 }
 
+function colorCube() {
+
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+}
+
+var modelViewMatrixLoc;
+var projectionMatrixLoc;
+var theta;
+var axis;
+var eye, at, up;
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var aspect = gl.canvas.width / gl.canvas.height;
+    var projectionMatrix = perspective(45.0, aspect, 0.1, 10.0);
+
+    theta[0] += 0.6;
+    theta[1] += 0.4;
+    theta[2] += 0.2;
+
+    var mv = lookAt(eye, at, up);
+    mv = mult(mv, rotate(theta[0], [1, 0, 0]));
+    mv = mult(mv, rotate(theta[1], [0, 1, 0]));
+    mv = mult(mv, rotate(theta[2], [0, 0, 1]));
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(mv));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
+
+    requestAnimFrame(render);
+}
